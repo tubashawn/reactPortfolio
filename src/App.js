98acec0e9fd4d90e8cbd35b2e69b002c24ca7d12
tubaShawn <shawnrmangum@gmail.com>
2018-10-14 22:21:40 -0600

    button test successful

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+          <Button className="btn-danger">Test</Button>
       </div>
     );
   }
